baf03909e52ef828443188cb31240b922de9f3c0
Andrew Oh <andrewoh531@gmail.com>
2017-06-12 21:31:39 +1000

    Running commands individually outside of bash scripts

--- .buildkite/deploy.sh
+++ .buildkite/deploy.sh
@@ -4,4 +4,4 @@ set -e
 working_dir="$( cd "$( dirname "${0}" )/.." && pwd )"
 
 echo "Deploying serverless functions..."
-docker run -v $(pwd):/opt/app -e AWS_DEFAULT_REGION -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY andrewoh531/docker-serverless serverless deploy
+docker run -v $(pwd):/opt/app -e AWS_DEFAULT_REGION -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY user-service:test serverless deploy
