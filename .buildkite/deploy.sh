7a8438d42f38ed986dce404ed30c66ddf97c1b7e
Andrew Oh <andrewoh531@gmail.com>
2017-06-12 21:43:06 +1000

    Using andrewoh531/docker-serverless as the base image and removing bash scripts

--- .buildkite/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-working_dir="$( cd "$( dirname "${0}" )/.." && pwd )"
-
-echo "Deploying serverless functions..."
-docker run -v $(pwd):/opt/app -e AWS_DEFAULT_REGION -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY user-service:test serverless deploy
